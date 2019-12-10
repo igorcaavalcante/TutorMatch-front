bcd3244ecb12d2c5344067374c8f547ccfb9b312
Igor Cavalcante <igorcavlim@gmail.com>
2019-12-10 12:59:50 -0300

    API call for tutors in home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { TutorsService } from "./services/tutors/tutors.service";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-    public ready = false;
+    private ready = false;
+    private tutors: any;
 
-    constructor() {
+    constructor(
+        private tutorsService: TutorsService
+    ) { }
 
-
+    async ngOnInit() {
+        this.tutors = (await this.tutorsService.get({ limit: 9, start: 0 })).result.data;
         this.ready = true;
     }
 }
